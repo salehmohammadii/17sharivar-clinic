2343a32ed8739379cea2a2eebb5d5ac6368c8a00
salehmohammadii <salehmohammadi475@gmail.com>
2023-06-21 12:01:51 +0330

    add button to add new one

--- templates/admin/paraclinic/index.php
+++ templates/admin/paraclinic/index.php
@@ -1,5 +1,7 @@
 <div class="tblBox w-100 h-100 overflow-auto ps-5 pe-5">
     <div class="card-body py-3">
+        <a class=" text-white  btn-success px-3 py-2 w-100px d-block ms-auto text-center me-5" style="border-radius: 5px;" href="<?= url('admin/paraclinic/create') ?>">افزودن  </a>
+
         <!--begin::Table container-->
         <div class="table-responsive">
             <!--begin::Table-->
